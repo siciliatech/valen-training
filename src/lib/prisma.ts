93ad9c9237e7eb30d1b98feffd6968573e3902d4
Valentina Vargas <valentinarociobelen2@gmail.com>
2026-03-12 23:10:15 -0300

    implementacion de mysql en docker y persistencia con prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,7 @@ dotenv.config();
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
